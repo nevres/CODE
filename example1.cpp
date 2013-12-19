592600837114c82e64546022dfae6166d9189649
nevres <nevres_jahic@hotmail.com>
2013-12-19 18:46:23 +0100

    added algo for caluculating maze
    added radom maze

--- example1.cpp
+++ example1.cpp
@@ -9,15 +9,21 @@
 #include <stdlib.h>     /* srand, rand */
 #include <time.h>    
 
+#define MAX 61  // 30 * 2 + 1
+#define CELL 900  // 30 * 30
+#define WALL 1
+#define PATH 0
+
 typedef Angel::vec4  color4;
 typedef Angel::vec4  point4;
 
+int maze[MAX][MAX];
 const int NumVertices = 4000;//(6 faces)(2 triangles/face)(3 vertices/triangle)
 
 point4 points[NumVertices];
 color4 colors[NumVertices];
 
-int maze[10][10];
+
 // RGBA olors
 color4 vertex_colors[8] = {
     color4( 0.0, 0.0, 0.0, 1.0 ),  // black
@@ -73,8 +79,114 @@ GLfloat Transform3;
 // quad generates two triangles for each face and assigns colors
 //    to the vertices
 int Index;
-void
 
+int is_closed(int maze[MAX][MAX], int x, int y)
+{
+    if(maze[x - 1][y]  == WALL
+       && maze[x][y - 1] == WALL
+       && maze[x][y + 1] == WALL
+       && maze[x + 1][y] == WALL
+    )
+        return 1;
+ 
+    return 0;
+}
+
+void init_maze(int maze[MAX][MAX])
+{
+     for(int a = 0; a < MAX; a++)
+     {
+         for(int b = 0; b < MAX; b++)
+         {
+             if(a % 2 == 0 || b % 2 == 0)
+                 maze[a][b] = 1;
+             else
+                 maze[a][b] = PATH;
+         }
+     }
+}
+ 
+void maze_generator(int indeks, int maze[MAX][MAX], int backtrack_x[CELL], int backtrack_y[CELL], int x, int y, int n, int visited)
+{
+    if(visited < n * n)
+    {
+        int neighbour_valid = -1;
+        int neighbour_x[4];
+        int neighbour_y[4];
+        int step[4];
+ 
+        int x_next;
+        int y_next;
+ 
+        if(x - 2 > 0 && is_closed(maze, x - 2, y))  // upside
+        {
+            neighbour_valid++;
+            neighbour_x[neighbour_valid]=x - 2;;
+            neighbour_y[neighbour_valid]=y;
+            step[neighbour_valid]=1;
+        }
+ 
+        if(y - 2 > 0 && is_closed(maze, x, y - 2))  // leftside
+        {
+            neighbour_valid++;
+            neighbour_x[neighbour_valid]=x;
+            neighbour_y[neighbour_valid]=y - 2;
+            step[neighbour_valid]=2;
+        }
+ 
+        if(y + 2 < n * 2 + 1 && is_closed(maze, x, y + 2))  // rightside
+        {
+            neighbour_valid++;
+            neighbour_x[neighbour_valid]=x;
+            neighbour_y[neighbour_valid]=y + 2;
+            step[neighbour_valid]=3;
+ 
+        }
+ 
+        if(x + 2 < n * 2 + 1 && is_closed(maze, x + 2, y))  // downside
+        {
+            neighbour_valid++;
+            neighbour_x[neighbour_valid]=x+2;
+            neighbour_y[neighbour_valid]=y;
+            step[neighbour_valid]=4;
+        }
+ 
+        if(neighbour_valid == -1)
+        {
+            // backtrack
+            x_next = backtrack_x[indeks];
+            y_next = backtrack_y[indeks];
+            indeks--;
+        }
+ 
+        if(neighbour_valid!=-1)
+        {
+            int randomization = neighbour_valid + 1;
+            int random = rand()%randomization;
+            x_next = neighbour_x[random];
+            y_next = neighbour_y[random];
+            indeks++;
+            backtrack_x[indeks] = x_next;
+            backtrack_y[indeks] = y_next;
+ 
+            int rstep = step[random];
+ 
+            if(rstep == 1)
+                maze[x_next+1][y_next] = PATH;
+            else if(rstep == 2)
+                maze[x_next][y_next + 1] = PATH;
+            else if(rstep == 3)
+                maze[x_next][y_next - 1] = PATH;
+            else if(rstep == 4)
+                maze[x_next - 1][y_next] = PATH;
+            visited++;
+        }
+ 
+        maze_generator(indeks, maze, backtrack_x, backtrack_y, x_next, y_next, n, visited);
+    }
+}
+
+void
 quad( point4 a, point4 b, point4 c, point4 d , int a1, int b1, int c1, int d1)
 {
     colors[Index] = vertex_colors[a1]; points[Index] = a; Index++;
@@ -90,42 +202,35 @@ void
 colorcube()
 {
 
-	for (int i = 0; i < 10; i++){
-		for (int j = 0; j < 10; j++){
-			maze[i][j] = rand() % 2;
-		}
-	}
-
+	
 	Index = 0;
 	int counter = 0;
 	int x = 0, y = 0;
 
-	for (GLfloat i = -1; i < 0.8; i+=0.2) // for y
+	for (GLfloat i = -1; i < 0.76; i+=0.16) // for y
 	{
 		x = 0;
-		for (GLfloat j = -1; j < 0.8; j+=0.2)// for x
+		for (GLfloat j = -1; j < 0.76; j+=0.16)// for x
 		{
 			//std::cout<<j<<"    "<<i<<std::endl;
 			point4 pointZero =point4( j, i,  0.1, 1.0 ); 
-			point4 pointOne = point4( j,  i +0.2,  0.1, 1.0 ); 
-			point4 pointTwo = point4(  j+0.2,  i+0.2,  0.1, 1.0 ); 
-			point4 pointTree =point4(  j+0.2, i,  0.1, 1.0 ); 
+			point4 pointOne = point4( j,  i +0.16,  0.1, 1.0 ); 
+			point4 pointTwo = point4(  j+0.16,  i+0.16,  0.1, 1.0 ); 
+			point4 pointTree =point4(  j+0.16, i,  0.1, 1.0 ); 
 			point4 pointFour =point4( j, i,  -0.1, 1.0 ); 
-			point4 pointFive =point4( j,  i+0.2,  -0.1, 1.0 ); 
-			point4 pointSix = point4(  j+0.2,  i+0.2,  -0.1, 1.0 ); 
-			point4 pointSeven=point4(  j+0.2, i,  -0.1, 1.0 ); 
+			point4 pointFive =point4( j,  i+0.16,  -0.1, 1.0 ); 
+			point4 pointSix = point4(  j+0.16,  i+0.16,  -0.1, 1.0 ); 
+			point4 pointSeven=point4(  j+0.16, i,  -0.1, 1.0 ); 
 
 			std::cout<<maze[x][y];
-			quad( pointOne, pointZero, pointTree, pointTwo, 1, 0, 3, 2 ); // front side  ok
+			quad( pointOne, pointZero, pointTree, pointTwo, 1, 0, 3, 2 ); // front side 
 			if(maze[x][y]){
 			quad( pointTwo, pointTree, pointSeven, pointSix, 2, 3, 7, 6 ); //right side
 			quad( pointTree, pointZero, pointFour, pointSeven, 3, 0, 4, 7 ); // down side
 			quad( pointSix, pointFive, pointOne, pointTwo, 6, 5, 1, 2 ); // up side
-			}
 			quad( pointFour, pointFive, pointSix, pointSeven, 4, 5, 6, 7 ); // back side   ok
-			if(maze[x][y])
 			quad( pointFive, pointFour, pointZero, pointOne, 5, 4, 0, 1 ); // left side
-			
+			}
 			x++;
 		}
 		std::cout<<std::endl;
@@ -302,7 +407,17 @@ int
 main( int argc, char **argv )
 {
 	srand (time(NULL));
-    glutInit( &argc, argv );
+	
+    int size = 5;
+    int indeks = 0;
+    int backtrack_x[CELL];
+    int backtrack_y[CELL];
+    init_maze(maze);
+    backtrack_x[indeks] = 1;
+    backtrack_y[indeks] = 1;
+    maze_generator(indeks, maze, backtrack_x, backtrack_y, 1, 1, size, 1);
+	
+	glutInit( &argc, argv );
     glutInitDisplayMode( GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH );
     glutInitWindowSize( 512, 512 );
     //glutInitContextVersion( 3, 2 );
