ffb3e6b35d490456dd68c493169afce904897fe5
nevres <nevres_jahic@hotmail.com>
2013-12-19 22:49:38 +0100

    changed path color

--- example1.cpp
+++ example1.cpp
@@ -27,7 +27,7 @@ color4 colors[NumVertices];
 
 
 // RGBA olors
-color4 vertex_colors[8] = {
+color4 vertex_colors[9] = {
     color4( 0.0, 0.0, 0.0, 1.0 ),  // black
     color4( 1.0, 0.0, 0.0, 1.0 ),  // red
     color4( 1.0, 1.0, 0.0, 1.0 ),  // yellow
@@ -35,7 +35,8 @@ color4 vertex_colors[8] = {
     color4( 0.0, 0.0, 1.0, 1.0 ),  // blue
     color4( 1.0, 0.0, 1.0, 1.0 ),  // magenta
     color4( 1.0, 1.0, 1.0, 1.0 ),  // white
-    color4( 0.0, 1.0, 1.0, 1.0 )   // cyan
+    color4( 0.0, 1.0, 1.0, 1.0 ),   // cyan
+	color4( 1.0, 1.0, 1.0, 1.0 ),
 };
 
 // Vertices of a unit cube centered at origin, sides aligned with axes
@@ -229,7 +230,7 @@ colorcube()
 
 
 			std::cout<<maze[x][y];
-			quad( pointOne, pointZero, pointTree, pointTwo, 1, 0, 3, 2 ); // front side 
+			quad( pointOne, pointZero, pointTree, pointTwo, 7, 7, 7, 7 ); // front side 
 			if(maze[x][y]){
 			quad( pointTwo, pointTree, pointSeven, pointSix, 2, 3, 7, 6 ); //right side
 			quad( pointTree, pointZero, pointFour, pointSeven, 3, 0, 4, 7 ); // down side
